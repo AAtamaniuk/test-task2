b7ee6ca0e185d8eb784137e3e65f14d4a18016f0
Oleksandr Atamaniuk <aatamaniuk.dev@gmail.com>
2019-05-22 22:04:45 +0300

    Updates

---
+++ precache-manifest.b556ab2180616fa4944c192ede6940ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c242f6a2206d5264ac4bf0e188d0c8ff",
+    "url": "/test-task2/index.html"
+  },
+  {
+    "revision": "1dfa309dc07afb180a7e",
+    "url": "/test-task2/static/css/main.80426dd9.chunk.css"
+  },
+  {
+    "revision": "c8fcbded823638a1b1ae",
+    "url": "/test-task2/static/js/2.4671aa27.chunk.js"
+  },
+  {
+    "revision": "1dfa309dc07afb180a7e",
+    "url": "/test-task2/static/js/main.d9ac1d9d.chunk.js"
+  },
+  {
+    "revision": "1743b509dd4970968603",
+    "url": "/test-task2/static/js/runtime~main.8a7e3fc2.js"
+  }
+]);
